62f1d24425f3ec14b3fd379998b97381a9c8f80e
SadCreeper <87826632@qq.com>
2017-02-25 16:48:53 +0800

    finish commit

--- resources/views/shared/comment.blade.php
+++ resources/views/shared/comment.blade.php
@@ -5,15 +5,8 @@
         <p>{{ $comment->created_at }}</p>
     </div>
     <div class="col-md-4">
-        @yield('link')
-        <!-- 编辑按钮 -->
-        <a class="btn btn-primary" href="{{ route('comments.edit', $comment->id) }}" style="display: inline-block;">编辑</a>
-        <!-- 删除按钮 -->
-        <form action="{{ route('comments.destroy', $comment->id) }}" method="post" style="display: inline-block;">
-            {{ csrf_field() }}
-            {{ method_field('DELETE') }}
-            <button type="submit" class="btn btn-danger">删除</button>
-        </form>
+        @yield('buttons')
+        
     </div>
 </div>
 
--- resources/views/shared/comment_admin.blade.php
+++ resources/views/shared/comment_admin.blade.php
@@ -1,6 +1,14 @@
 @extends('shared.comment')
 
-@section('link')
+@section('buttons')
 <!-- 跳转按钮 -->
 <a class="btn btn-primary" href="{{ route('articles.show', $comment->article->id) }}" style="display: inline-block;">跳转文章</a>
+<!-- 编辑按钮 -->
+<a class="btn btn-primary" href="{{ route('comments.edit', $comment->id) }}" style="display: inline-block;">编辑</a>
+<!-- 删除按钮 -->
+<form action="{{ route('comments.destroy', $comment->id) }}" method="post" style="display: inline-block;">
+    {{ csrf_field() }}
+    {{ method_field('DELETE') }}
+    <button type="submit" class="btn btn-danger">删除</button>
+</form>
 @endsection
